838d32622d5097ae4666da2bdc35be1ac5deb6fb
Max Lepin <max.09@outloook.com>
2020-10-01 17:34:58 +0200

    first commit

---
+++ src/main/java/TrayIconDemo.java
@@ -0,0 +1,109 @@
+
+/*
+ * TrayIconDemo.java
+ */
+
+import java.awt.*;
+import java.awt.event.*;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.URL;
+import java.net.UnknownHostException;
+import javax.swing.*;
+
+class TrayIconDemo {
+    public static void main(String[] args) throws InterruptedException, IOException {
+        final PopupMenu popup = new PopupMenu();
+        final TrayIcon trayIcon =
+                new TrayIcon(createImage("images/bulb.gif", "tray icon"));
+        final SystemTray tray = SystemTray.getSystemTray();
+        trayIcon.setImageAutoSize(true);
+        Image green = createImage("images/green_dot.gif", "green icon");
+        Image red = createImage("images/red_dot.gif", "red icon");
+        /* Use an appropriate Look and Feel */
+        try {
+            UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+            //UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
+        } catch (UnsupportedLookAndFeelException ex) {
+            ex.printStackTrace();
+        } catch (IllegalAccessException ex) {
+            ex.printStackTrace();
+        } catch (InstantiationException ex) {
+            ex.printStackTrace();
+        } catch (ClassNotFoundException ex) {
+            ex.printStackTrace();
+        }
+        /* Turn off metal's use of bold fonts */
+        UIManager.put("swing.boldMetal", Boolean.FALSE);
+        //Schedule a job for the event-dispatching thread:
+        //adding TrayIcon.
+
+        //Check the SystemTray support
+        if (!SystemTray.isSupported()) {
+            System.out.println("SystemTray is not supported");
+            return;
+        }
+
+        // Create a popup menu components
+        MenuItem aboutItem = new MenuItem("About");
+        MenuItem exitItem = new MenuItem("Exit");
+
+        //Add components to popup menu
+        popup.add(aboutItem);
+        popup.addSeparator();
+        popup.add(exitItem);
+
+        trayIcon.setPopupMenu(popup);
+
+        try {
+            tray.add(trayIcon);
+        } catch (AWTException e) {
+            System.out.println("TrayIcon could not be added.");
+            return;
+        }
+        exitItem.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                tray.remove(trayIcon);
+                System.exit(0);
+            }
+        });
+
+        while (true) {
+            Thread.sleep(1000);
+            if (receivedPingFromGoogle()){
+                System.out.println("Connected");
+                trayIcon.setImage(green);
+            } else {
+                System.out.println("Offline");
+                trayIcon.setImage(red);
+
+            };
+        }
+    }
+
+
+    //Obtain the image URL
+    protected static Image createImage(String path, String description) {
+        URL imageURL = TrayIconDemo.class.getResource(path);
+
+        if (imageURL == null) {
+            System.err.println("Resource not found: " + path);
+            return null;
+        } else {
+            return (new ImageIcon(imageURL, description)).getImage();
+        }
+    }
+
+    public static boolean receivedPingFromGoogle() throws IOException
+    {
+        try {
+            InetAddress google = InetAddress.getByName("www.google.com");
+            return google.isReachable(1000);
+        } catch (UnknownHostException e){
+            return false;
+        } catch (SocketException e){
+            return false;
+        }
+    }
+}
